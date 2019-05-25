56eaf5470a087ee6528533d2ceed0049df9f1c05
ziwei2221 <48003373+ziwei2221@users.noreply.github.com>
2019-05-25 14:09:36 +0800

    Add files via upload

--- wordFrequencyAndPlotGraph.py
+++ wordFrequencyAndPlotGraph.py
@@ -15,7 +15,8 @@ def sentiment():
 city = {1:"Brasilia", 2:"NewYork", 3:"London", 4:"Bangkok", 5:"Kabul", 6:"Tokyo"}
 #print(city)
 # city_chosen = int(input("Choose a city by key in its number"))
-city_chosen = 1
+city_chosen = 2
+
 
 #specify path
 ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -33,19 +34,33 @@ for x in files:
     i +=1
 
 
-articles[1].calculateWords()    #preprocessing for article 1
+
+articles[0].calculateWords()    #preprocessing for article 1
+print("article pos word is " + str(articles[0].getPosCount()))
+print("article neg word is " + str(articles[0].getNegCount()))
+print("polarity is " + str(articles[0].getPolarity()))
+articles[1].calculateWords()    #preprocessing for article 2
 print("article pos word is " + str(articles[1].getPosCount()))
 print("article neg word is " + str(articles[1].getNegCount()))
 print("polarity is " + str(articles[1].getPolarity()))
-
+articles[2].calculateWords()    #preprocessing for article 3
+print("article pos word is " + str(articles[2].getPosCount()))
+print("article neg word is " + str(articles[2].getNegCount()))
+print("polarity is " + str(articles[2].getPolarity()))
+articles[3].calculateWords()    #preprocessing for article 4
+print("article pos word is " + str(articles[3].getPosCount()))
+print("article neg word is " + str(articles[3].getNegCount()))
+print("polarity is " + str(articles[3].getPolarity()))
+articles[4].calculateWords()    #preprocessing for article 5
+print("article pos word is " + str(articles[4].getPosCount()))
+print("article neg word is " + str(articles[4].getNegCount()))
+print("polarity is " + str(articles[4].getPolarity()))
 print(sentiment())
+
 
 N=5
 ori_word = (int(articles[0].getOriTotal()), int(articles[1].getOriTotal()),int(articles[2].getOriTotal()),int(articles[3].getOriTotal()),int(articles[4].getOriTotal()))
 stop_word = (int(articles[0].getNoStopTotal()), int(articles[1].getNoStopTotal()),int(articles[2].getNoStopTotal()), int(articles[3].getNoStopTotal()), int(articles[4].getNoStopTotal()))
-
-pos_word = (int(articles[0].getPosCount()),int(articles[1].getPosCount()),int(articles[2].getPosCount()),int(articles[3].getPosCount()),int(articles[4].getPosCount()))
-neg_word = (int(articles[0].getNegCount()),int(articles[1].getNegCount()),int(articles[2].getNegCount()),int(articles[3].getNegCount()),int(articles[4].getNegCount()))
 
 index = np.arange(N)
 width=0.35
@@ -56,6 +71,10 @@ plt.ylabel("Word")
 plt.title("Number of Word")
 plt.xticks(index + width / 2, ('Article 1', 'Article 2', 'Article 3', 'Article 4', 'Article 5'))
 plt.legend(loc='best')
+
+pos_word = (int(articles[0].getPosCount()),int(articles[1].getPosCount()),int(articles[2].getPosCount()),int(articles[3].getPosCount()),int(articles[4].getPosCount()))
+neg_word = (int(articles[0].getNegCount()),int(articles[1].getNegCount()),int(articles[2].getNegCount()),int(articles[3].getNegCount()),int(articles[4].getNegCount()))
+
 
 plt.subplot(2,1,2)
 plt.bar(index,pos_word,width,label="Positive Word Count")
